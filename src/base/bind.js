c968b7f7e1a445fbae424d5a27b52c660e96c09a
guizimo <17680262548@163.com>
2022-11-13 19:16:42 +0800

    feat：优化bind方法显示

--- src/base/bind.js
+++ src/base/bind.js
@@ -1,18 +1,22 @@
+/**
+ * 手写bind
+ * @returns {function(): any}
+ */
 Function.prototype.myBind = function () {
+  // 处理函数
   let args = Array.from(arguments);
   let thisArg = args.shift();
+  // 暂存this
   let thisFunc = this;
-
   // 因为需要构造函数，所以不能是匿名函数了
-  let fBound = function () {
-    newArgs = args.concat(Array.from(arguments));
+  const fBound = function () {
+    const newArgs = args.concat(Array.from(arguments));
     // 判断是否为构造函数
     thisArg = this instanceof fBound ? this : thisArg;
     return thisFunc.apply(thisArg, newArgs);
   }
-
   // 直接将原函数的prototype赋值给绑定函数
   fBound.prototype = this.prototype;
-
+  // 返回
   return fBound;
 }
