88294648e2da0dae17653be8eb45923ae558543a
guizimo <17680262548@163.com>
2023-07-17 17:10:28 +0800

    feat: 新增jest库

--- src/base/call.js
+++ src/base/call.js
@@ -1,22 +1,20 @@
 /**
  * 自定义实现call
- * @param thisArg
- * @param args
  * @returns {*|void}
  */
-Function.prototype.myCall = function (thisArg, ...args) {
+Function.prototype.myCall = function () {
   // 判断this的指向
   if (typeof this !== 'function') {
     throw new Error('type error')
   }
-  // 处理外部传递过来的this，如果this为null时，指向全局的window对象
-  const newThis = thisArg || window
-  // 创建一个Null对象，用作返回的载体
-  let res = null
+  // 处理参数，拿到传入的this与参数
+  const args = Array.from(arguments)
+  // 如果传入的this为null时，指向全局的window对象
+  const newThis = args.shift() || window
   // 创建一个临时属性接受this，注意唯一性，可能会覆盖原有的属性
   newThis.fn = this
   // 将参数作为属性置入
-  res = newThis.fn(...args)
+  const res = newThis.fn(...args)
   // 删除属性，防止污染
   delete newThis.fn
   // 返回
@@ -26,22 +24,22 @@ Function.prototype.myCall = function (thisArg, ...args) {
 
 /**
  * 自定义实现call，优化版
- * @param thisArg
- * @param args
  * @returns {*}
  */
-Function.prototype.myCallPlus = function (thisArg, ...args) {
+Function.prototype.myCallPlus = function () {
   // 判断this的指向
   if (typeof this !== 'function') {
     throw new Error('type error')
   }
-  // 处理外部传递过来的this，如果this为null时，指向全局的window对象
-  const newThis = thisArg || window
+  // 处理参数，拿到传入的this与参数
+  const args = Array.from(arguments)
+  // 如果传入的this为null时，指向全局的window对象
+  const newThis = args.shift() || window
   // 创建一个唯一的key
   let fn = Symbol()
   // 接收临时this
   newThis[fn] = this
-  // 传入参数，调用新属性
+  // 将参数作为属性置入
   const res = newThis[fn](...args)
   // 删除属性，防止污染
   delete newThis[fn]
